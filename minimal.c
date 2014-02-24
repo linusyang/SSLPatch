19efcdc3045940dfb697c192cefb0446a49ce698
Linus Yang <laokongzi@gmail.com>
2014-02-24 16:44:24 +0800

    Move interpose code

--- minimal.c
+++ minimal.c
@@ -42,7 +42,26 @@
  */
 
 #include "minimal.h"
-#include <AssertMacros.h>
+
+// Interpose
+#define CSSMOID_SHA1WithRSA (*_CSSMOID_SHA1WithRSA)
+#define CSSMOID_SHA256WithRSA (*_CSSMOID_SHA256WithRSA)
+#define CSSMOID_SHA384WithRSA (*_CSSMOID_SHA384WithRSA)
+
+#define SSLHashMD5 (*_SSLHashMD5)
+#define SSLHashSHA1 (*_SSLHashSHA1)
+#define SSLHashSHA256 (*_SSLHashSHA256)
+#define SSLHashSHA384 (*_SSLHashSHA384)
+
+#define SSLAllocBuffer (*_SSLAllocBuffer)
+#define SSLFreeBuffer (*_SSLFreeBuffer)
+#define SSLDecodeInt (*_SSLDecodeInt)
+#define sslFreePubKey (*_sslFreePubKey)
+#define sslGetPubKeyFromBits (*_sslGetPubKeyFromBits)
+#define ReadyHash (*_ReadyHash)
+#define SSLDecodeDHKeyParams (*_SSLDecodeDHKeyParams)
+#define sslRsaVerify (*_sslRsaVerify)
+#define sslRawVerify (*_sslRawVerify)
 
 static OSStatus
 SSLVerifySignedServerKeyExchange(SSLContext *ctx, bool isRsa, SSLBuffer signedParams,
